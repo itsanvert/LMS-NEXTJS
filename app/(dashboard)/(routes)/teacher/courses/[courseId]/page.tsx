4e2cc9bc2ab62bd8dc5cb350c9f78e3f296bebea
Vert San <itsanvert@gmail.com>
2025-02-28 04:36:30 +0700

    first commit

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,5 +1,7 @@
+import { IconBadge } from "@/components/ui/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
+import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
 
 const CourseIdPage = async ({ params }: { params: { courseId?: string } }) => {
@@ -45,6 +47,14 @@ const CourseIdPage = async ({ params }: { params: { courseId?: string } }) => {
           <span className="text-sm text-slate-700">
             Complete all fields {completionText}
           </span>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} />
+            <h2 className="text-xl">Customer your course</h2>
+          </div>
         </div>
       </div>
     </div>
